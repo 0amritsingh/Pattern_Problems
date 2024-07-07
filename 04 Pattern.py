6eac22b4e2f7d20fcce95abef527ae2a1db512e8
Amrit Singh <amritsingh72007@gmail.com>
2024-07-07 10:11:49 +0530

    managed time complexity

--- 04 Pattern.py
+++ 04 Pattern.py
@@ -1,5 +1,5 @@
 n = int(input('>>> '))
-for i in range(n + 1):
-    for j in range(i):
-        print(i, end=' ')
+for i in range(n):
+    for j in range(i + 1):
+        print(i + 1, end=' ')
     print()
