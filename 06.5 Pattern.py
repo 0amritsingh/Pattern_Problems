7e8fd9cfdbc06fd33c0a2dda68b92445c7105ed8
Amrit Singh <amritsingh72007@gmail.com>
2025-04-04 23:13:02 +0530

    Yes we are back
    after a long time we are now back. I'll be updating my journey on x.com.
    
    However, created new file 06.5 and 07 and solved 2 new star patterns and revised privious 6 patterns

--- 06.5 Pattern.py
+++ 06.5 Pattern.py
@@ -3,7 +3,5 @@ for i in range (n):
     for j in range(n - i):
         print(" ", end="")
     for k in range(i + 1):
-        print("*", end="")  
-    for l in range(i):
         print("*", end="")    
     print()  
