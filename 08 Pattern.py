fb8edec4b00272a2b6822e63759c2ec8087184c2
Amrit Singh <amritsingh72007@gmail.com>
2025-04-05 22:41:42 +0530

    Deleting empty files
    Making some changes so that new patterns can be added

--- 08 Pattern.py
+++ 08 Pattern.py
@@ -1,9 +1,9 @@
 n = int(input('>>> '))
 for i in range (n):
-    for j in range(i):
-        print(' ', end='')
-    for k in range(n - i):
-        print("*", end="")
-    for l in range((n - i) - 1):
-        print("*", end="")
-    print()   
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(i + 1):
+        print("*", end="")  
+    for l in range(i):
+        print("*", end="")    
+    print()  
--- 09 Pattern.py
+++ 09 Pattern.py
@@ -1,1 +1,9 @@
-# Code is arriving soon :)
+n = int(input('>>> '))
+for i in range (n):
+    for j in range(i):
+        print(' ', end='')
+    for k in range(n - i):
+        print("*", end="")
+    for l in range((n - i) - 1):
+        print("*", end="")
+    print()   
